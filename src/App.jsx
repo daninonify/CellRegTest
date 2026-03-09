5b16b401e1ade93eb88b23b79aa37777bce3dcda
daninonify <daninonify@gmail.com>
2026-03-09 13:09:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
